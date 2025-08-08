1c82d8643341f45a9dbdb7cfd7a810bcb6395869
Tekano Meso <tekanonetflorist01@gmail.com>
2025-08-08 11:30:32 +0200

    _app.tsx

---
+++ alx-project-0x07/pages/_app.tsx
@@ -0,0 +1,13 @@
+// pages/_app.tsx
+import type { AppProps } from 'next/app';
+import Layout from '../components/layouts/Layout';
+import '../styles/globals.css'; // Import global styles (if you have any)
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
+}
+
